bf6d44578d5f278b783d679a888b13e0c848f43c
Paulo Alves <paulo1rm23@gmail.com>
2017-10-25 14:26:29 -0300

    Footer

--- components/footer.php
+++ components/footer.php
@@ -1,5 +1,14 @@
 		<footer>
-			
+			<div class="container">
+				<span class="endereco"><i class="fa fa-map-marker" aria-hidden="true"></i> Rua Antomar de Brito Freitas nº 3678 - Candelária</span>
+				<span class="endereco"><i class="fa fa-phone" aria-hidden="true"></i> (84) 3205-1051 | 3205-1884</span>
+
+				<div class="redes-sociais">
+					<h4>Confira nossas<br /><span>REDES SOCIAIS</span></h4>
+					<a href="" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+					<a href="" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+				</div><!-- .redes-sociais -->
+            </div><!-- .container -->
 		</footer>
 
 		<script type='text/javascript' src='assets/js/jquery.js'></script>
